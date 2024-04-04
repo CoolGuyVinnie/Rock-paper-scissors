67d13315157e33e9d4f9efa78d0ae666a031787b
Vincent <Nguyen.Vincent11@Gmail.com>
2024-04-03 19:48:41 -0700

    Finished wins in players case

--- rpsScript.js
+++ rpsScript.js
@@ -1,9 +1,18 @@
 function playRound(playerSelection, computerSelection) {
-    if ( (playerSelection == "paper") && (computerSelection == "rock") ) {
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You win! Rock beats scissors.";
+    }
+
+    if (playerSelection == "paper" && computerSelection == "rock") {
         return "You win! Paper beats rock.";
     }
+
+    if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You win! Scissors beats paper.";
+    }
+
 }
 
 const playerSelection = (prompt("Make your choice").toLowerCase())
-const computerSelection = "rock";
+const computerSelection = "scissors";
 console.log( (playRound(playerSelection, computerSelection)));
